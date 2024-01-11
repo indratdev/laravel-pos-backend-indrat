9a02ff229e8ca119c3d4820be9bcab3048b23818
indratdev <indrat.dev@gmail.com>
2024-01-11 16:15:43 +0700

    update product controller api

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -26,7 +26,37 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        $data = $request->all();
+        $request->validate([
+            'name' => 'required|min:3',
+            'price' => 'required|integer',
+            'working_time' => 'required|integer',
+            'category' => 'required|in:satuan,kiloan',
+            'image' => 'image|mimes:png,jpg,jpeg'
+        ]);
+
+        // $filename = time() . '.' . $request->image->extension();
+        $filename = date("Ymd").time(). '.' . $request->image->extension();
+        $request->image->storeAs('public/products', $filename);
+        $product = \App\Models\Product::create([
+            'name' => $request->name,
+            'price' => (int) $request->price,
+            'working_time' => (int) $request->stock,
+            'category' => $request->category,
+            'image' => $filename,
+        ]);
+
+        if ($product) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Product Created',
+                'data' => $product
+            ], 201);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Product Failed to Save',
+            ], 409);
+        }
     }
 
     /**
